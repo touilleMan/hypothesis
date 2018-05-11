3208a9ce38734f2cb23e777ad3212653da16468f
David R. MacIver <david@drmaciver.com>
2018-05-11 08:11:05 +0100

    Fix RST tests for real

--- whole-repo-tests/test_rst_is_valid.py
+++ whole-repo-tests/test_rst_is_valid.py
@@ -18,31 +18,25 @@
 from __future__ import division, print_function, absolute_import
 
 import os
-from glob import glob
-
-import pytest
 
 import hypothesistooling as tools
 from hypothesistooling.scripts import pip_tool
 
-ROOT_RST = glob(os.path.join(tools.ROOT, '*.rst'))
-GUIDES = glob(os.path.join(tools.ROOT, 'guides', '*.rst'))
-PYTHON_DOCS = os.path.join(tools.HYPOTHESIS_PYTHON, '*.rst')
+
+def is_sphinx(f):
+    f = os.path.abspath(f)
+    return f.startswith(os.path.join(tools.HYPOTHESIS_PYTHON, 'docs'))
 
 
-def test_covers_all_rst_files():
-    all_rst = {
-        f for f in tools.all_files() if f.endswith('.rst') and
-        os.path.basename(f) != 'RELEASE.rst'
-    }
-    assert set(ROOT_RST + GUIDES + PYTHON_DOCS) == all_rst
+ALL_RST = [
+    f for f in tools.all_files()
+    if os.path.basename(f) != 'RELEASE.rst' and f.endswith('.rst')
+]
 
 
-@pytest.mark.parametrize('f', ROOT_RST + GUIDES)
-def test_passes_rst_lint(f):
-    pip_tool('rst-lint', f)
+def test_passes_rst_lint():
+    pip_tool('rst-lint', *[f for f in ALL_RST if not is_sphinx(f)])
 
 
-@pytest.mark.parametrize('f', ROOT_RST + GUIDES + PYTHON_DOCS)
-def test_passes_flake8(f):
-    pip_tool('flake8', '--select=W191,W291,W292,W293,W391', f)
+def test_passes_flake8():
+    pip_tool('flake8', '--select=W191,W291,W292,W293,W391', *ALL_RST)
